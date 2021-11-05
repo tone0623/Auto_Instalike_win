e6697107dd33c89837f01b52e4dd52493f0a5e56
tone0623 <tone0623@icloud.com>
2021-11-05 13:53:00 +0900

    管理対象変更

--- Return_Value.py
+++
@@ -1,9 +0,0 @@
-def return_pass():
-    user_name = "ここにユーザネーム"
-    password = "ここにパスワード"
-    return (user_name, password)
-
-
-def return_tag():
-    taglist = ["ここにハッシュタグ", "ここにハッシュタグ", "ここにハッシュタグ"]
-    return taglist
